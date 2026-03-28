43afa83d121f5cf45da2e44c23327a33307d2920
sanodzegg <sanodzegg@gmail.com>
2026-03-28 19:12:33 +0400

    update theme toggle responsive

--- src/components/theme/theme-toggle.tsx
+++ src/components/theme/theme-toggle.tsx
@@ -9,9 +9,10 @@ export function ThemeToggle() {
     <Button
       variant={'outline'}
       size="icon"
+      className="2xl:size-10"
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
     >
-      {theme === 'dark' ? <Sun className="h-4 w-4" /> : <Moon className="h-4 w-4" />}
+      {theme === 'dark' ? <Sun className="size-4 2xl:size-5" /> : <Moon className="size-4 2xl:size-5" />}
     </Button>
   )
 }
